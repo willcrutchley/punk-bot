1ed667566a1ee920ea561421767a9c963d852d4e
Will Crutchley <crutchley.william@gmail.com>
2018-07-18 23:55:03 +0100

    admin

--- more-testing.py
+++ more-testing.py
@@ -3,26 +3,48 @@ import json, pickle
 with open("recipes.json", "r") as infile:
     data = json.load(infile)
 
-index = 0
+n = 0
 outputs = []
 
 choice = input("Generate new list? Y/n")
 if choice.lower() == 'y':
     while True:
         try:
-            outputs.append(data["recipes"][index]["outputItem"])
-            print(data["recipes"][index]["outputItem"]) 
-            index += 1
+            outputs.append(data["recipes"][n]["outputItem"])
+            print(data["recipes"][n]["outputItem"]) 
+            n += 1
         except:
             break
-    with open('outfile', 'wb') as fp:
+    with open('outputs', 'wb') as fp:
         pickle.dump(outputs, fp)
 elif choice.lower() == 'n':
-    with open ('outfile', 'rb') as fp:
+    with open ('outputs', 'rb') as fp:
         outputs = pickle.load(fp)
+else:
+    print("Invalid response")
+    exit
 desired = input("What item?")
 desired = desired.upper()
 if desired in outputs:
     index = outputs.index(desired)
 else:
     print("Invalid output item")
+    exit
+
+inputs = data["recipes"][index]["inputs"]
+
+n = 0
+inputItems = []
+inputQuantities = []
+while True:
+    try:
+        inputItems.append(inputs[n]["inputItem"])
+        inputQuantities.append(inputs[n]["inputQuantity"])
+        n += 1
+    except:
+        break
+
+for item in inputItems:
+    print(item)
+    print(inputQuantities[inputItems.index(item)])
+
