9a03c3f3ade82ac4cea97d05fdf1a08ceca91461
Will Crutchley <crutchley.william@gmail.com>
2018-07-19 12:44:58 +0100

    more selectors

--- test-bot_3.7.py
+++ test-bot_3.7.py
@@ -15,12 +15,10 @@ async def on_message(message):
         helpDistances = "b!distances - display distances inbetween planets on a server"
         helpPortals = "b!portals - display portal requirements"
         helpUniverse = "b!universe - display a map of the universe, and the punk stargates"
-        description = "**1.-**`" + helpDistances + "`\n**2.-**`" + helpPortals + "`\n**3.-**`" + helpUniverse + "`\n**4.-**`b!help - show this message`\n**5.-**`!help - list all of the help sections`"
+        description = "**1.- **`" + helpDistances + "`\n**2.- **`" + helpPortals + "`\n**3.- **`" + helpUniverse + "`\n**4.- **`b!help - show this message`\n**5.- **`!help - list all of the help sections`"
         embed = discord.Embed(description = description, colour = 0xBECCE0)
         embed.set_author(name = title, icon_url = avatar_url)
-        await client.send_message(message.channel, embed = embed)
-        embed = discord.Embed(colour = 0xE2B660)
-        embed.set_author(name = "Enter a number to execute that command.", icon_url = avatar_url)
+        embed.set_footer(text="Enter a number to execute that command.")
         await client.send_message(message.channel, embed = embed)
         response = await client.wait_for_message(author = sender, timeout = 60)
         response = int(response.content)
@@ -47,12 +45,10 @@ async def on_message(message):
 
     async def info():
         title = 'The different help sections are:'
-        description = "**1.-**`b!help - help for the Boundless section of the bot`"
+        description = "**1.- **`b!help - help for the Boundless section of the bot`"
         embed = discord.Embed(description = description, colour = 0xBECCE0)
         embed.set_author(name = title, icon_url = avatar_url)
-        await client.send_message(message.channel, embed = embed)
-        embed = discord.Embed(colour = 0xE2B660)
-        embed.set_author(name = "Enter a number to execute that command.", icon_url = avatar_url)
+        embed.set_footer(text="Enter a number to execute that command.")
         await client.send_message(message.channel, embed = embed)
         lines = int(description.count("\n") + 1)
         response = await client.wait_for_message(author = sender, timeout = 60)
@@ -103,9 +99,22 @@ async def on_message(message):
                 await client.send_message(message.channel, embed = embed)
     
         except:
-            title = "Error!"
+            title = "Incorrect usage!"
             description = "The correct usage is `b!recipe outputItem`"
             embed = discord.Embed(description = description, colour = 0xDC5A46)
+            embed.set_author(name = title, icon_url = avatar_url)
+            await client.send_message(message.channel, embed = embed)
+
+    async def bdistance(server):
+            title = "Here are the distances between planets on Boundless' " + server + " server:"
+            embed = discord.Embed(colour = 0xAE86C4)
+            if server == "live":
+                id_server = "469227663803023370/"
+            elif server == "testing":
+                id_server = "469227666512281605/"
+            else:
+                id_server = "469227665296195586/"
+            embed.set_image(url="https://cdn.discordapp.com/attachments/469227496018149406/"+ id_server +"distances_" + server + ".png")
             embed.set_author(name = title, icon_url = avatar_url)
             await client.send_message(message.channel, embed = embed)
 
@@ -113,34 +122,45 @@ async def on_message(message):
         args = message.content.split(" ")
         try:
             if args[1] == "live":
-                title = "Here are the distances between planets on Boundless' live server:"
-                embed = discord.Embed(colour = 0xAE86C4)
-                embed.set_author(name = title, icon_url = avatar_url)
-                embed.set_image(url="https://cdn.discordapp.com/attachments/469227496018149406/469227663803023370/distances_live.png")
-                await client.send_message(message.channel, embed = embed)
+                await bdistance("live")
+                return
             elif args[1] == "testing":
-                title = "Here are the distances between planets on Boundless' testing server:"
-                embed = discord.Embed(colour = 0xAE86C4)
-                embed.set_author(name = title, icon_url = avatar_url)
-                embed.set_image(url="https://cdn.discordapp.com/attachments/469227496018149406/469227666512281605/distances_testing.png")
-                await client.send_message(message.channel, embed = embed)
+                await bdistance("testing")
+                return
             elif args[1] == "staging":
-                title = "Here are the distances between planets on Boundless' staging server:"
-                embed = discord.Embed(colour = 0xAE86C4)
-                embed.set_image(url="https://cdn.discordapp.com/attachments/469227496018149406/469227665296195586/distances_staging.png")
-                embed.set_author(name = title, icon_url = avatar_url)
-                await client.send_message(message.channel, embed = embed)
+                await bdistance("staging")
+                return
             else:
                 title = "Error!"
                 description = "These distances haven't been uploaded yet, or that Boundless server doesn't exist."
                 embed = discord.Embed(description = description, colour = 0xDC5A46)
                 embed.set_author(name = title, icon_url = avatar_url)
                 await client.send_message(message.channel, embed = embed)
+                return
+            return
         except:
-            description = "`b!distances serverName`"
+            description = "**1.- **`live`**\n2.- **`testing`**\n3.- **`staging`"
             embed = discord.Embed(description = description, colour = 0xBECCE0)
-            embed.set_author(name = "The correct usage is:", icon_url = avatar_url)
+            embed.set_author(name = "Select a server:", icon_url = avatar_url)
+            embed.set_footer(text="Enter a number to select that server.")
             await client.send_message(message.channel, embed = embed)
+            response = await client.wait_for_message(author = sender, timeout = 60)
+            response = int(response.content)
+            lines = int(description.count("\n") + 1)
+            if int(response) <= int(lines):
+                if response == 1:
+                    await bdistance("live")
+                elif response == 2:
+                    await bdistance("testing")
+                elif response == 3:
+                    await bdistance("staging")
+            else:
+                title = "Error!"
+                description = "There is no server corresponding to that number."
+                embed = discord.Embed(description = description, colour = 0xDC5A46)
+                embed.set_author(name = title, icon_url = avatar_url)
+                await client.send_message(message.channel, embed = embed)
+            
 
     async def bportals():
         title = "Here are the portal requirements in Boundless:"
@@ -236,10 +256,10 @@ async def on_message(message):
     if message.content.lower().startswith('b!help') or message.content.lower().startswith('b!info'):
         await binfo()
 
-    if message.content.lower().startswith('b!distances'):
+    if message.content.lower().startswith('b!distance'):
         await bdistances()
 
-    if message.content.lower().startswith('b!portals'):
+    if message.content.lower().startswith('b!portal'):
         await bportals()
 
     if message.content.lower().startswith('b!universe'):
